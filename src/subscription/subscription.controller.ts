12ba13f967681690ef03dee47f29d15eeeb639f2
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2024-12-09 16:21:42 +0100

    added cache for subscriptions

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -1,21 +1,25 @@
-import { Controller, Get, Body, Logger, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Logger, Post, UseGuards, Inject } from '@nestjs/common';
 import { SubscriptionOdooService } from "./odoo.service";
 import { AuthenticatedUser, AuthGuard } from 'nest-keycloak-connect';
 import { UserDto } from './user.dto';
-
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 @Controller('subscription')
 export class SubscriptionController {
   private readonly logger = new Logger(SubscriptionController.name);
 
-  constructor(private subscriptionOdooService: SubscriptionOdooService) {}
+  constructor(private subscriptionOdooService: SubscriptionOdooService, @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
   @Get()
   @UseGuards(AuthGuard)
   async getSubscription(
     @AuthenticatedUser() user: UserDto,
   ) {
-    console.log(user.email);
-    return await this.subscriptionOdooService.checkUserSubscription(user.email);
+    const cache = await this.cacheManager.get(user.email);
+    if (cache) return cache;
+    const subscription = await this.subscriptionOdooService.checkUserSubscription(user.email);
+    await this.cacheManager.set(user.email, subscription);
+    return subscription;
   }
 
   @Get('plans')
